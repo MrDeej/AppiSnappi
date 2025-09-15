48d4e69cb74d56f35b5bc0c6aada8c70ea2f284f
Eirik Lohne Bakken <Eirik.Lohne.Bakken@xxl.no>
2025-09-15 09:11:49 +0200

    remove stupid test

--- src/FamilyApplication.AspireApp.Tests/WebTests.cs
+++ src/FamilyApplication.AspireApp.Tests/WebTests.cs
@@ -2,26 +2,26 @@ namespace FamilyApplication.AspireApp.Tests;
 
 public class WebTests
 {
-    [Test]
-    public async Task GetWebResourceRootReturnsOkStatusCode()
-    {
-        // Arrange
-        var appHost = await DistributedApplicationTestingBuilder.CreateAsync<Projects.FamilyApplication_AspireApp_AppHost>();
-        appHost.Services.ConfigureHttpClientDefaults(clientBuilder =>
-        {
-            clientBuilder.AddStandardResilienceHandler();
-        });
+    //[Test]
+    //public async Task GetWebResourceRootReturnsOkStatusCode()
+    //{
+    //    // Arrange
+    //    var appHost = await DistributedApplicationTestingBuilder.CreateAsync<Projects.FamilyApplication_AspireApp_AppHost>();
+    //    appHost.Services.ConfigureHttpClientDefaults(clientBuilder =>
+    //    {
+    //        clientBuilder.AddStandardResilienceHandler();
+    //    });
 
-        await using var app = await appHost.BuildAsync();
-        var resourceNotificationService = app.Services.GetRequiredService<ResourceNotificationService>();
-        await app.StartAsync();
+    //    await using var app = await appHost.BuildAsync();
+    //    var resourceNotificationService = app.Services.GetRequiredService<ResourceNotificationService>();
+    //    await app.StartAsync();
 
-        // Act
-        var httpClient = app.CreateHttpClient("webfrontend");
-        await resourceNotificationService.WaitForResourceAsync("webfrontend", KnownResourceStates.Running).WaitAsync(TimeSpan.FromSeconds(30));
-        var response = await httpClient.GetAsync("/");
+    //    // Act
+    //    var httpClient = app.CreateHttpClient("webfrontend");
+    //    await resourceNotificationService.WaitForResourceAsync("webfrontend", KnownResourceStates.Running).WaitAsync(TimeSpan.FromSeconds(30));
+    //    var response = await httpClient.GetAsync("/");
 
-        // Assert
-        Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
-    }
+    //    // Assert
+    //    Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+    //}
 }
